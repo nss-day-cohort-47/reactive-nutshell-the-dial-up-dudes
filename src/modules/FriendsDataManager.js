df28d06a98a528f3527b8f48b16308d49c2ef534
Autumn Fotopoulos <acarte94@gmail.com>
2021-04-14 21:22:16 -0500

    working on cards and forms

--- src/modules/FriendsDataManager.js
+++ src/modules/FriendsDataManager.js
@@ -2,10 +2,11 @@ const remoteURL= "http://localhost:8088"
 
 export const getFriendById = (id) => {
     return fetch(`${remoteURL}/friends/${id}`)
+    .then(res =>res.json())
 }
 
 export const getAllFriends= () => { 
-    return fetch(`${remoteURL}/friends`)
+    return fetch(`${remoteURL}/friends?_expand=user`)
     .then(result => result.json())
 
 }
@@ -22,5 +23,17 @@ export const addFriend = (newFriend) => {
 export const deleteFriend = (id) => {
     return fetch(`${remoteURL}/friends/${id}`,{
         method: "DELETE"
-    }).then(response => response.json())
+    }).then(result => result.json())
 }
+
+export const updateFriend= (editFriend) => {
+    return fetch(`${remoteURL}/friends/${editFriend.id}`, {
+        method:"PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(editFriend)
+    }).then(res=> res.json())
+}
+
+// difference between result and response?
