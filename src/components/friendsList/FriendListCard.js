94a336ce90a1de4ba60c686f2b9afde92cb2e942
Autumn Fotopoulos <acarte94@gmail.com>
2021-04-16 09:58:14 -0500

    editing

--- src/components/friendsList/FriendListCard.js
+++ src/components/friendsList/FriendListCard.js
@@ -4,11 +4,11 @@ export const FriendListCard = ({ friend, handleDeleteFriend }) => {
     return (
         <div className="friend_card">
             <div className="friendcard-content">
-           <div className= "profilepic">{friend.user?.image}</div>
+           <img src={friend.user?.image} alt="friend"/>
                 <h3 className="friend__name">Name: {friend.user?.name}</h3>
                 
                 <button type="button" onClick={() =>
-                handleDeleteFriend(friend.id)}>Baiii</button>
+                handleDeleteFriend(friend.id)}>See ya later!</button>
             </div>
         </div>
     )
